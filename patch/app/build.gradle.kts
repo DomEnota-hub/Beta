42877a3cd882b1c9361fbc6ada7671dc9b418e4a
DomEnota-hub <domenota6212@gmail.com>
2026-09-23 09:09:44 +0000

    Canonicalize VL80S equipment crosslinks

--- patch/app/build.gradle.kts
+++ patch/app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "ru.railbrake.calculator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 189
-        versionName = "1.2.2-dev14-beta44"
+        versionCode = 190
+        versionName = "1.2.2-dev14-beta45"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
